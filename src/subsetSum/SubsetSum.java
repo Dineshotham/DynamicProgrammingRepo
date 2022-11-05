2f32fc808adfaeaf266968aacc971662ac15be81
Dineshotham Kumar Khambhammettu <dk185173@ncr.com>
2022-11-05 18:46:19 +0530

    SubSetSum Tabulation

--- src/subsetSum/SubsetSum.java
+++ src/subsetSum/SubsetSum.java
@@ -14,11 +14,40 @@ public class SubsetSum {
             return isSubSetSumRecursiveBackTracking(arr,subSetsum-arr[n-1],n-1) || //Inclusion Path
                     isSubSetSumRecursiveBackTracking(arr,subSetsum,n-1); //exclusion Path
     }
+    //Recursive code ends
+
+    //Tabulation approach
+    public static boolean isSubSetSumTabulation(int arr[],int subSetsum){
+        int n=arr.length;
+        boolean dp[][]=new boolean[n+1][subSetsum+1];
+
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=subSetsum;j++){
+                if(i==0){
+                    dp[i][j]=false;
+                }
+                else if(j==0){
+                   dp[i][j]=true;
+                }
+                else if(arr[i-1]==j)
+                    dp[i][j]=true;
+                else if(arr[i-1]>j){
+                    dp[i][j]=dp[i-1][j];
+                }else{
+                    dp[i][j]=(dp[i-1][j]||dp[i-1][j-arr[i-1]]);
+                }
+            }
+        }
+
+
+        return dp[n][subSetsum];
+    }
 
     public static void main(String ar[]){
 
-        int arr[]={1,2,5}; //Given array
+        int arr[]={1,2,1,3}; //Given array
         int sum=5;  //Given Subset Sum
         System.out.println(isSubSetSumRecursiveBackTracking(arr,sum,arr.length));
+        System.out.println(isSubSetSumTabulation(arr,sum));
     }
 }
